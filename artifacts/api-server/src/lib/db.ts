5a5899b81481e54e8e52d7799db92899b67471ab
ppowerands <sambousman819@gmail.com>
2026-05-21 09:23:31 +0100

    Update db.ts

--- artifacts/api-server/src/lib/db.ts
+++ artifacts/api-server/src/lib/db.ts
@@ -1,1 +1,23 @@
-export { db } from "../../lib/db/src/index";
+import {
+  db,
+  ordersTable,
+  orderItemsTable,
+  cartItemsTable,
+  productsTable,
+  productVariantsTable,
+  addonsTable,
+  deliveryZonesTable,
+  usersTable,
+} from "../../lib/db/src/index";
+
+export {
+  db,
+  ordersTable,
+  orderItemsTable,
+  cartItemsTable,
+  productsTable,
+  productVariantsTable,
+  addonsTable,
+  deliveryZonesTable,
+  usersTable,
+};
