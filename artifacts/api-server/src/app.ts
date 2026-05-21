32e8a366f4276f557c165845754365551a56e7c0
ppowerands <sambousman819@gmail.com>
2026-05-21 01:45:26 +0100

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,13 +1,9 @@
-import express, { type Express } from "express";
-import cors from "cors";
-import router from "./routes";
+import express from "express";
 
-const app: Express = express();
+const app = express();
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", router);
+app.get("/", (_req, res) => {
+  res.json({ ok: true });
+});
 
 export default app;
